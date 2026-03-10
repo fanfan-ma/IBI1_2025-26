d799ae7a96e5088acf3bfabb122a9e857851739d
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-03-10 14:45:24 +0800

    write the code to calculate

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,4 +1,14 @@
-# give the initial number	of infected	students and the growth	rate over 24 hrs
+# give the initial number of infected students and the growth rate over 24 hrs
 # write the formula for calculating the number of newly infected students each day
 # reassign the original variable to represent the current number of infected students, print it
 # compare this value with 91. If it >= 91, terminate the loop
+i = 0 # to record the date
+a = 5 # the initial number of infected students is 5
+r = 0.4 # the growth rate is 0.4
+for a <= 91:
+    a += 0.4^a
+    i += 1
+    print("The number of infected students is" , a , "at day" ,i)
+print("it takes" , i , "days to infect all")
+
+    
