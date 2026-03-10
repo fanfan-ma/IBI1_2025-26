1fa6c90ddb1ea308ddab6d5af4739afee84e2a69
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-03-10 12:40:13 +0800

    write the pseudocode

---
+++ Practical4/infection.py
@@ -0,0 +1,4 @@
+# give the initial number	of infected	students and the growth	rate over 24 hrs
+# write the formula for calculating the number of newly infected students each day
+# reassign the original variable to represent the current number of infected students, print it
+# compare this value with 91. If it >= 91, terminate the loop
