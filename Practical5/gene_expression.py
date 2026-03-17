1a1997de8f90a858f1e48990abcafc676b413c1c
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-03-17 11:01:50 +0800

    create d new dir and create a file called gene_expression.py and create dict

---
+++ Practical5/gene_expression.py
@@ -0,0 +1,25 @@
+# create a dictionary called dict_genes
+# add another gene into this directory
+
+dict_genes = {
+    "gene1": {
+        "name": "TP53"
+        "expression": "12.4"
+    }
+    "gene2": {
+        "name": "EGFR"
+        "expression": "15.1"
+    }
+    "gene1": {
+        "name": "BRCA1"
+        "expression": "8.2"
+    }
+    "gene1": {
+        "name": "PTEN"
+        "expression": "5.3"
+    }
+    "gene1": {
+        "name": "ESR1"
+        "expression": "10.7"
+    }
+}
