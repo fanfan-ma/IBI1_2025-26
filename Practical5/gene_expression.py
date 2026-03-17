a62909c1501a5948325ac2ed6c1830b86fe3e4ac
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-03-17 19:43:47 +0800

    write the dictionary in a simpler way

--- Practical5/gene_expression.py
+++ Practical5/gene_expression.py
@@ -3,31 +3,13 @@
 # install the installation package, then import and test it
 
 dict_genes = {
-    "gene1": {
-        "name": "TP53", # pay attention that the mapping relationships in the dict should be separated by commas
-        "expression": 12.4
-    }, # whatever the value type is (even also a dict), there should be a comma
-    "gene2": {
-        "name": "EGFR",
-        "expression": 15.1
-    },
-    "gene3": {
-        "name": "BRCA1",
-        "expression": 8.2
-    },
-    "gene4": {
-        "name": "PTEN",
-        "expression": 5.3
-    },
-    "gene5": {
-        "name": "ESR1",
-        "expression": 10.7
+    "TP53": 12.4, # pay attention that the mapping relationships in the dict should be separated by commas
+    "EGFR": 15.1,
+    "BRCA1": 8.2,
+    "PTEN": 5.3,
+    "ESR1": 10.7
     }
-}
-dict_genes["gene6"] = { # add another gene into the dict
-    "name": "MYC",
-    "expression": 11.6
-}
+dict_genes["MYC"] = 11.6 # add another gene into the dict
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -40,12 +22,12 @@ import matplotlib.pyplot as plt
 # give the title of x-axis and y-axis respectively
 # give the distance between the coordinates, make sure that on the x-axis, it is the same as the positions of the bars
 
-N = 6
+N = len(dict_genes)
 genes = []
 expressions = []
-for gene_reference in dict_genes.values():
-    genes.append(gene_reference["name"])
-    expressions.append(gene_reference["expression"])
+for gene , expression in dict_genes.items():
+    genes.append(gene)
+    expressions.append(expression)
 width = 0.35
 x_coordinate = np.arange(N) # np.arange means generating a sequence of consecutive numbers starting from 0 and ending at N
 pl = plt.bar(x_coordinate , expressions , width) # the contents in the brackets represent the position, height and width of the bars respectively
@@ -54,3 +36,12 @@ plt.title("Expressions by gene")
 plt.xticks(x_coordinate , genes) # display the numerical positions as label names
 plt.yticks(np.arange(0 , 20 , 2)) # the contents in the brackets represent the start point, end point and step of the y-axis respectively
 plt.show()
+
+# create a variable called gene_of_interest
+# jugde if it is one of the names in the dictionary
+# if yes, print its expression
+# if no, print an error message
+
+# gene_of_interest = TP53 # this could be modified
+# if gene_of_interest in genes:
+#     print("the expression is" ,  )
