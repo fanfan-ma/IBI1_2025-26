9f8bdc5184b24387a48accfd1ff717a38e1fdb16
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-03-17 11:06:50 +0800

    change the value type of the expression into float

--- Practical5/gene_expression.py
+++ Practical5/gene_expression.py
@@ -4,22 +4,22 @@
 dict_genes = {
     "gene1": {
         "name": "TP53"
-        "expression": "12.4"
+        "expression": 12.4
     }
     "gene2": {
         "name": "EGFR"
-        "expression": "15.1"
+        "expression": 15.1
     }
     "gene3": {
         "name": "BRCA1"
-        "expression": "8.2"
+        "expression": 8.2
     }
     "gene4": {
         "name": "PTEN"
-        "expression": "5.3"
+        "expression": 5.3
     }
     "gene5": {
         "name": "ESR1"
-        "expression": "10.7"
+        "expression": 10.7
     }
 }
