1fca960d59a3a071a067b341cbc1bfcf3cc74dc9
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-03-24 09:46:50 +0800

    record the initial number

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -2,9 +2,10 @@
 # write the formula for calculating the number of newly infected students each day
 # reassign the original variable to represent the current number of infected students, print it
 # compare this value with 91. If it >= 91, terminate the loop
-i = 0 # to record the date
+i = 1 # to record the date
 a = 5 # the initial number of infected students is 5
 r = 0.4 # the growth rate is 0.4
+print("The number of infected students is" , a , "at day" ,i) # recording the initial number
 while a < 91:
     a += r*a
     i += 1
