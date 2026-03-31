08153ee8c85f9d3c44390cea54dac216a7ecfb8a
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-04-01 00:34:42 +0800

    identify sequences which contain at least one in-frame stop codon

---
+++ Practical7/stop_codons.py
@@ -0,0 +1,35 @@
+# open the origin file
+# open the new, empty file
+# create a variable called sequence and make it an empty string (to store the gene lines)
+# get into the for loop and get all the lines respectively
+# judge if the line is the head line (start with >)
+# if yes, get the gene name and write it into the new FASTA file, update the sequence to empty string
+# if no, join the lines together, which means, cut the \n and add to the sequence
+# find the start code in the new string after concentration
+# judge if the string has in-frame stop codons (this is done when the loop get into next HEAD line)
+# if yes, write the whole string into the new FASTA file
+# if no, continue to the next line
+
+input_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa' , 'r')
+output_file = open('stop_genes.fa' , 'w')
+sequence = ""
+import re
+for line in input_file:
+    if re.search(r'^>' , line): 
+        if re.search(r'ATG(?:...)*?(?:TAA|TAG|TGA)' , sequence):
+            stop_codons = re.findall(r'ATG(?:...)*(TAA|TAG|TGA)' , sequence)
+            output_file.write(",".join(stop_codons) + '\n')
+            for i in range(0, len(sequence), 60):
+                output_file.write(sequence[i:i+60] + '\n')
+        gene_name = re.findall(r'>(\S*)' , line)
+        output_file.write('>' + gene_name[0] + ';') # get the first string in the list
+        sequence = ""
+    else:
+        sequence += line.rstrip()
+if re.search(r'ATG(?:...)*?(?:TAA|TAG|TGA)' , sequence):
+    stop_codons = re.findall(r'ATG(?:...)*?(TAA|TAG|TGA)' , sequence)
+    output_file.write(",".join(stop_codons) + '\n')
+    for i in range(0, len(sequence), 60):
+        output_file.write(sequence[i:i+60] + '\n')
+input_file.close()
+output_file.close()
