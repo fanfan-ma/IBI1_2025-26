699536c00fe71bbc05fa8186c27c4306cd8cfa3b
Ma Shufan <shufan.25@intl.zju.edu.cn>
2026-04-08 20:50:46 +0800

    ensure that the test program will only be executed when directly run this file and transfer the function of judgement to the function

--- Practical8/protein_mass_predictor.py
+++ Practical8/protein_mass_predictor.py
@@ -1,6 +1,4 @@
 # create a dictionary to store the amu of each AA
-# traverse each AA, then calculate its quantity, and subsuquently calculate its quantity
-# return the final mass
 
 amino_acid_dict = {
         "G": 57.02,
@@ -24,25 +22,33 @@ amino_acid_dict = {
         "Y": 163.06,
         "W": 186.08
     }
-while True:
-    amino_acid_sequence = input("Please enter the amino acid sequence of the protein: ")
-    valid = True
-    for amino_acid in amino_acid_sequence:
-        if amino_acid not in amino_acid_dict.keys():
-            print("Error: amino acid not found")
-            valid = False
-            break
-    if not valid: # if the input is legal, then do not go to the continue step, if it is illegal, continue and input again
-        continue
-    break # exit the loop
+
+# define a function to calculate mass and judge if the suquence is valid
+
 def calculating_protein_mass(amino_acid_sequence , amino_acid_dict):
     """
     Input: a sequence of amino acid
-    Return: the mass of the protein
+    Return: the mass of the protein (if the amino acid does not exist, return None)
     """
     mass = 0
     for amino_acid in amino_acid_sequence:
-        mass += amino_acid_dict[amino_acid]
+        if amino_acid not in amino_acid_dict.keys():
+            return None
+        else:
+            mass += amino_acid_dict[amino_acid]
     return mass
-protein_mass = calculating_protein_mass(amino_acid_sequence , amino_acid_dict)
-print(f"The mass of the total protein in atomic mass units is {protein_mass}.")
+
+# get into a loop to get valid input, use the function to judge and calculate
+
+if __name__ == "__main__": # ensure that the test program will only be executed when directly run this file
+    while True:
+        amino_acid_sequence = input("Please enter the amino acid sequence of the protein: ")
+        valid = True
+        protein_mass = calculating_protein_mass(amino_acid_sequence , amino_acid_dict)
+        if protein_mass == None:
+            print("Error: amino acid not found")
+            valid = False
+        if not valid: # if the input is legal, then do not go to the continue step, if it is illegal, continue and input again
+            continue
+        print(f"The mass of the total protein in atomic mass units is {protein_mass:.2f}.")
+        break # exit the loop
